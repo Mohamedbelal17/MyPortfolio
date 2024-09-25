29553743499204e6c29afc9bbf11ce36ef91b233
Mohamed Belal <111300150+Mohamedbelal17@users.noreply.github.com>
2024-09-25 20:20:57 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/Mohamedbelal17/MyPortfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
